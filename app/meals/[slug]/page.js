9104cd1bdf3777859708d84beb5f964ce206aea1
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-02-19 00:22:30 +0100

    Special meal page created

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import { getMeal } from '@/lib/meals';
+import styles from './page.module.css'
+import Image from 'next/image';
 
-const MealsDetails = () => {
+
+const MealsDetails = async ({params}) => {
+
+  const meal = await getMeal(params.slug)
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+  
   return (
-    <h1>
-      Share Meal
-    </h1>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal?.image} alt={meal?.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>
+            {meal.title}
+          </h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}  ></p>
+      </main>
+    </>
   )
 }
 
