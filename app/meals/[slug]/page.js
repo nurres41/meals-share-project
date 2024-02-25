72e7728c802f9d9d0a461499b2a61c9218ebd065
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-02-25 22:16:23 +0100

    Project done. Except AWS for image

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,6 +3,19 @@ import styles from './page.module.css'
 import Image from 'next/image';
 import { notFound } from 'next/navigation'
 
+export async function generateMetadata({ params }){
+  const meal = getMeal(params.mealSlug)
+
+  if(!meal){
+    notFound()
+  }
+  
+  return{
+    title: meal.title,
+    description: meal.summary
+  }
+}
+
 const MealsDetails = async ({params}) => {
 
   const meal = await getMeal(params.slug)
--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Delicious meals, shared by a food-loving community.',  
+}
+
 const Meals = async () => {
   const meals = await getMeals();
   
